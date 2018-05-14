0219fef6f61497815c38ec45c1ca268ebf457d63
smartman <smartman2014@qq.com>
2018-05-14 09:18:45 +0800

    scaffold: update app title

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
     jasmine: true,
   },
   rules: {
+    'linebreak-style': ['error', 'windows'],
     'generator-star-spacing': [0],
     'consistent-return': [0],
     'react/forbid-prop-types': [0],
