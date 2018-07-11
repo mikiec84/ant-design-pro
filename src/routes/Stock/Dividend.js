af58c21806f25d2793559b5ff0090704eaf2304b
kingofhawks <kingofhawks@qq.com>
2018-07-11 22:17:39 +0800

    add 分红页面

--- src/routes/Stock/Dividend.js
+++ src/routes/Stock/Dividend.js
@@ -2,18 +2,53 @@ import React, { PureComponent} from 'react';
 import { connect } from 'dva';
 import {
   Card,
+  Button,
+  Form,
+  Modal,
+  Input,
 } from 'antd';
 import StandardTable from 'components/StandardTable';
 import PageHeaderLayout from '../../layouts/PageHeaderLayout';
 
 import styles from './Portfolio.less';
+import { message } from 'antd/lib/index';
 // import { message } from 'antd/lib/index';
 
+const FormItem = Form.Item;
 const getValue = obj =>
   Object.keys(obj)
     .map(key => obj[key])
     .join(',');
 
+const CreateForm = Form.create()(props => {
+  const { modalVisible, form, handleAdd, handleModalVisible } = props;
+  const okHandle = () => {
+    form.validateFields((err, fieldsValue) => {
+      if (err) return;
+      form.resetFields();
+      handleAdd(fieldsValue);
+    });
+  };
+  return (
+    <Modal
+      title="新增分红"
+      visible={modalVisible}
+      onOk={okHandle}
+      onCancel={() => handleModalVisible()}
+    >
+      <FormItem labelCol={{ span: 5 }} wrapperCol={{ span: 15 }} label="日期">
+        {form.getFieldDecorator('code', {
+          rules: [{ required: true, message: 'Please input some description...' }],
+        })(<Input placeholder="请输入" />)}
+      </FormItem>
+      <FormItem labelCol={{ span: 5 }} wrapperCol={{ span: 15 }} label="分红金额">
+        {form.getFieldDecorator('money', {
+          rules: [{ required: true, message: 'Please input some description...' }],
+        })(<Input placeholder="请输入" />)}
+      </FormItem>
+    </Modal>
+  );
+});
 // #5 must define here to connect model data
 @connect(({ dividend, loading }) => ({
   dividend,
@@ -89,11 +124,32 @@ export default class Dividend extends PureComponent {
     });
   };
 
+  handleModalVisible = flag => {
+    this.setState({
+      modalVisible: !!flag,
+    });
+  };
+
+  handleAdd = fields => {
+    this.props.dispatch({
+      type: 'dividend/add',
+      payload: {
+        description: fields.desc,
+      },
+    });
+
+    message.success('添加成功');
+    this.setState({
+      modalVisible: false,
+    });
+  };
 
   render() {
+    console.log(this.props);
+    console.log(this.props.dividend.data.list);
     // #5
     const { dividend: { data }, loading } = this.props;
-    const { selectedRows} = this.state;
+    const { selectedRows, modalVisible} = this.state;
     const columns = [
       {
         title: '日期',
@@ -105,11 +161,21 @@ export default class Dividend extends PureComponent {
       },
     ];
 
+    const parentMethods = {
+      handleAdd: this.handleAdd,
+      handleModalVisible: this.handleModalVisible,
+    };
+
     return (
       <PageHeaderLayout title="分红">
 
         <Card bordered={false}>
           <div className={styles.tableList}>
+            <div className={styles.tableListOperator}>
+              <Button icon="plus" type="primary" onClick={() => this.handleModalVisible(true)}>
+                新建
+              </Button>
+            </div>
             <StandardTable
               selectedRows={selectedRows}
               loading={loading}
@@ -120,6 +186,7 @@ export default class Dividend extends PureComponent {
             />
           </div>
         </Card>
+        <CreateForm {...parentMethods} modalVisible={modalVisible} />
       </PageHeaderLayout>
     );
   }
